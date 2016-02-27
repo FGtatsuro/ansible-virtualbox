02899f43ea6ddf0da3b29507953877ec02553763
FGtatsuro <fujiistorage@gmail.com>
2016-02-27 13:35:27 +0900

    Add spec on Debian.

--- spec/virtualbox_spec.rb
+++ spec/virtualbox_spec.rb
@@ -4,6 +4,10 @@ describe package('virtualbox'), :if => os[:family] == 'darwin' do
   it { should be_installed.by('homebrew_cask') }
 end
 
+describe package('virtualbox'), :if => os[:family] == 'debian' do
+  it { should be_installed.by('apt') }
+end
+
 describe command('which VBoxManage') do
   its(:exit_status) { should eq 0 }
 end

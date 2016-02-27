d745a1784819ad424ee2d2d337db65fc501e8967
FGtatsuro <fujiistorage@gmail.com>
2016-02-28 00:21:42 +0900

    Fix package name on Debian.

--- spec/virtualbox_spec.rb
+++ spec/virtualbox_spec.rb
@@ -4,7 +4,7 @@ describe package('virtualbox'), :if => os[:family] == 'darwin' do
   it { should be_installed.by('homebrew_cask') }
 end
 
-describe package('virtualbox'), :if => os[:family] == 'debian' do
+describe package('virtualbox-5.0'), :if => os[:family] == 'debian' do
   it { should be_installed.by('apt') }
 end
 
